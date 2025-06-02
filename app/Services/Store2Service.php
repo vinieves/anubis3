7541267af7190ebb0af8751a2425d5052d7f4be5
vinieves <viniciusgow@hotmail.com>
2025-06-01 23:52:50 -0300

    correção dados enviados

--- app/Services/Store2Service.php
+++ app/Services/Store2Service.php
@@ -24,6 +24,17 @@ class Store2Service
         file_put_contents($nomesPath, implode(PHP_EOL, $nomes));
         logger()->info('Nome sorteado para STORE2', ['nome' => $nomeNovo]);
 
+        // Separa nome e sobrenome
+        $nomeParts = explode(' ', $nomeNovo, 2);
+        $firstName = $nomeParts[0];
+        $lastName = isset($nomeParts[1]) ? $nomeParts[1] : '';
+
+        // Gera telefone e phoneCode aleatórios usando a lógica do CartPandaService
+        $faker = fake('en_US');
+        $phone = $faker->phoneNumber();
+        $phone = preg_replace('/[^0-9]/', '', $phone);
+        $phoneCode = '1';
+
         // 2. Gera e-mail random com base no nome
         $cartPandaService = new CartPandaService();
         $emailNovo = $cartPandaService->generateEmail($nomeNovo);
@@ -33,28 +44,36 @@ class Store2Service
         $checkoutIdStore2 = env('CHECKOUT_ID_STORE2');
         logger()->info('CheckoutId usado para STORE2', ['checkoutId' => $checkoutIdStore2]);
 
-        // 4. Monta o comando para rodar o bot2.js
+        // 4. Monta o comando para rodar o bot2.js (igual ao CartPandaService)
         $process = new Process([
             'node',
             base_path('scripts/bot2.js'),
             $checkoutIdStore2,
-            $nomeNovo,
+            $firstName,
+            $lastName,
             $emailNovo,
+            $phone,
+            $phoneCode,
             $cardData['cardNumber'],
             $cardData['cardMonth'],
             $cardData['cardYear'],
             $cardData['cardCvv'],
+            env('CONNECTION_URL'),
         ]);
 
         logger()->info('Executando bot2.js para STORE2', [
             'args' => [
                 $checkoutIdStore2,
-                $nomeNovo,
+                $firstName,
+                $lastName,
                 $emailNovo,
+                $phone,
+                $phoneCode,
                 $cardData['cardNumber'],
                 $cardData['cardMonth'],
                 $cardData['cardYear'],
                 $cardData['cardCvv'],
+                env('CONNECTION_URL'),
             ]
         ]);
 
